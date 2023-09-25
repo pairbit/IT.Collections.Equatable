6dfc67326a0be715018baf41d3e4bc604a297b93
Ivan Tikhonov <itikhonov@mail.com>
2023-09-25 18:50:49 +0300

    add queue and stack

---
+++ IT.Collections.Equatable/EquatableQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IT.Collections.Equatable;
+
+public class EquatableQueue<T> : Queue<T>, IEquatable<EquatableQueue<T>>
+{
+    private readonly IEqualityComparer<T> _comparer;
+
+    public EquatableQueue(IEqualityComparer<T>? comparer = null)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public EquatableQueue(int capacity, IEqualityComparer<T>? comparer = null) : base(capacity)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public EquatableQueue(IEnumerable<T> collection, IEqualityComparer<T>? comparer = null) : base(collection)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public IEqualityComparer<T> Comparer => _comparer;
+
+    public override bool Equals(object? obj) => Equals(obj as EquatableQueue<T>);
+
+    public bool Equals(EquatableQueue<T>? other) => ReferenceEquals(this, other) || other is not null && this.SequenceEqual(other, _comparer);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        foreach (var item in this)
+        {
+            hash.Add(item, _comparer);
+        }
+        return hash.ToHashCode();
+    }
+}
---
+++ IT.Collections.Equatable/EquatableStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IT.Collections.Equatable;
+
+public class EquatableStack<T> : Stack<T>, IEquatable<EquatableStack<T>>
+{
+    private readonly IEqualityComparer<T> _comparer;
+
+    public EquatableStack(IEqualityComparer<T>? comparer = null)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public EquatableStack(int capacity, IEqualityComparer<T>? comparer = null) : base(capacity)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public EquatableStack(IEnumerable<T> collection, IEqualityComparer<T>? comparer = null) : base(collection)
+    {
+        _comparer = comparer ?? EqualityComparer<T>.Default;
+    }
+
+    public IEqualityComparer<T> Comparer => _comparer;
+
+    public override bool Equals(object? obj) => Equals(obj as EquatableStack<T>);
+
+    public bool Equals(EquatableStack<T>? other) => ReferenceEquals(this, other) || other is not null && this.SequenceEqual(other, _comparer);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        foreach (var item in this)
+        {
+            hash.Add(item, _comparer);
+        }
+        return hash.ToHashCode();
+    }
+}
