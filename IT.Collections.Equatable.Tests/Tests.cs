5ad717cd8ce437d80b29f23d44380cd6fc7583a0
Ivan Tikhonov <itikhonov@mail.com>
2023-10-23 16:49:57 +0300

    first release

--- IT.Collections.Equatable.Tests/Tests.cs
+++ IT.Collections.Equatable.Tests/Tests.cs
@@ -1,17 +1,77 @@
 using IT.Collections.Equatable.Factory;
 using IT.Collections.Factory;
 using IT.Collections.Factory.Factories;
+using System;
+using System.Collections.Generic;
 
 namespace IT.Collections.Equatable.Tests;
 
 public class Tests
 {
-    private IEnumerableFactoryRegistry _registry;
+    private IEnumerableFactoryRegistry _registry = null!;
+
+    record MyRecord<T>
+    {
+        public IEnumerable<T>? Enumerable { get; set; }
+    }
+
+    record SimpleRecord
+    {
+        public EquatableList<string>? List { get; set; }
+    }
 
     [SetUp]
     public void Setup()
     {
         _registry = new EnumerableFactoryRegistry().RegisterFactoriesEquatable();
+    }
+
+    [Test]
+    public void SimpleRecordTest()
+    {
+        var record1 = new SimpleRecord
+        {
+            List = new EquatableList<string> { "a", "b", "c" }
+        };
+
+        var record2 = new SimpleRecord
+        {
+            List = new EquatableList<string> { "a", "b", "c" }
+        };
+
+        Assert.That(record1.GetHashCode(), Is.EqualTo(record2.GetHashCode()));
+        Assert.That(record1, Is.EqualTo(record2));
+
+        var record3 = new SimpleRecord
+        {
+            List = new EquatableList<string>(StringComparer.OrdinalIgnoreCase)
+            { "test", "abc", "8" }
+        };
+
+        var record4 = new SimpleRecord
+        {
+            List = new EquatableList<string>(StringComparer.OrdinalIgnoreCase)
+            { "TeSt", "aBc", "8" }
+        };
+
+        Assert.That(record1.GetHashCode(), Is.Not.EqualTo(record3.GetHashCode()));
+        Assert.That(record2, Is.Not.EqualTo(record4));
+
+        Assert.That(record3.GetHashCode(), Is.EqualTo(record4.GetHashCode()));
+        Assert.That(record3, Is.EqualTo(record4));
+
+        var record5 = new SimpleRecord
+        {
+            List = new EquatableList<string>(StringComparer.OrdinalIgnoreCase)
+        };
+
+        var record6 = new SimpleRecord
+        {
+            List = new EquatableList<string>(StringComparer.Ordinal)
+        };
+
+        Assert.That(record5.GetHashCode(), Is.Not.EqualTo(record6.GetHashCode()));
+        Assert.That(record5, Is.Not.EqualTo(record6));
     }
 
     [Test]
@@ -104,6 +164,12 @@ public class Tests
 
         Assert.That(x.Equals(y), Is.True);
         Assert.That(y.Equals(x), Is.True);
+
+        var myrecord = new MyRecord<T> { Enumerable = x };
+        var myrecord2 = new MyRecord<T> { Enumerable = y };
+
+        Assert.That(myrecord.GetHashCode(), Is.EqualTo(myrecord2.GetHashCode()));
+        Assert.That(myrecord.Equals(myrecord2), Is.True);
     }
 
     private void SequenceNotEqual<T>(IEnumerable<T> x, IEnumerable<T> y)
@@ -112,6 +178,12 @@ public class Tests
 
         Assert.That(x.Equals(y), Is.False);
         Assert.That(y.Equals(x), Is.False);
+
+        var myrecord = new MyRecord<T> { Enumerable = x };
+        var myrecord2 = new MyRecord<T> { Enumerable = y };
+
+        Assert.That(myrecord.GetHashCode(), Is.Not.EqualTo(myrecord2.GetHashCode()));
+        Assert.That(myrecord.Equals(myrecord2), Is.False);
     }
 
     static bool flag;
